f8cb0ef27f610d5075d9895d5a0f47a1006887a7
Colleen <cmlohr@outlook.com>
2020-11-25 20:43:51 -0600

    setup for state game

---
+++ us-states-game-start/main.py
@@ -0,0 +1,32 @@
+import pandas
+import turtle
+
+screen = turtle.Screen()
+screen.title("U.S.A States Game")
+image = "blank_states_img.gif"
+screen.addshape(image)
+turtle.shape(image)
+# Code to get the x and y values of the states
+# def get_mouse_click_coor(x, y):
+#     print(x, y)
+#
+# turtle.onscreenclick(get_mouse_click_coor)
+# turtle.mainloop()
+
+answer_state = screen.textinput(title="Guess the state", prompt="Name a state:")
+
+
+
+# game_on = True
+# while game_on:
+#
+
+
+
+
+
+
+
+
+
+# screen.exitonclick()
