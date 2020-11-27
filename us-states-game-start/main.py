839d996159403820919ec385a98b5204a72ff4cf
Colleen <cmlohr@outlook.com>
2020-11-26 20:00:17 -0600

    updated scoring

--- us-states-game-start/main.py
+++ us-states-game-start/main.py
@@ -8,7 +8,7 @@ words.color("red")
 words.hideturtle()
 screen.setup(width=730, height=500)
 screen.bgcolor("black")
-screen.title("U.S. States Game")
+screen.title("US States Game")
 image = "blank_states_img.gif"
 screen.addshape(image)
 turtle.shape(image)
@@ -26,15 +26,17 @@ FONT = ("System", 10, "normal")
 ALIGN = "center"
 
 data = pandas.read_csv("50_states.csv")
-state = data["state"].tolist()
+raw_state = data["state"].tolist()
+state = [item.lower() for item in raw_state]
 print(state)
 correct_answer = 0
 game_on = True
 while game_on:
     num_of_states = len(state)
-    user_input = screen.textinput(title=f"{correct_answer}/{num_of_states}", prompt="Name a state:")
+    user_input = screen.textinput(title=f"{correct_answer}/50", prompt="Name a state:")
+    print(user_input)
     answer = user_input.capitalize()
-    if answer in state:
+    if user_input in state:
         row_data = data[data.state == f"{answer}"]
         x_value = int(row_data.x)
         y_value = int(row_data.y)
@@ -42,8 +44,7 @@ while game_on:
         words.goto(x_value, y_value)
         words.write(f"{answer}", font=FONT, align=ALIGN)
         correct_answer += 1
-        state.remove(answer)
-    if len(state) == 0:
+    if correct_answer == 50:
         words.write("GAME OVER")
         game_on = False
 
